7b15ca5ec09a1bac196f08bd4b522dc2f6ecb1eb
Don-Duong Quach <geekrelief@gmail.com>
2008-12-13 17:56:49 -0800

    Added timings sample

---
+++ sample/timings/TS.hx
@@ -0,0 +1,172 @@
+
+    import haxe.io.Bytes;
+
+    import org.amqp.Connection;
+    import org.amqp.ConnectionParameters;
+    import org.amqp.SessionManager;
+    import org.amqp.headers.BasicProperties;
+    import org.amqp.impl.SessionStateHandler;
+    import org.amqp.methods.basic.Publish;
+    import org.amqp.util.Properties;
+
+    import neko.vm.Thread;
+    import neko.vm.Deque;
+    import haxe.io.BytesInput;
+    import haxe.io.BytesOutput;
+
+    import org.amqp.BasicConsumer;
+    import org.amqp.Command;
+    import org.amqp.LifecycleEventHandler;
+    import org.amqp.ProtocolEvent;
+    import org.amqp.methods.basic.Consume;
+    import org.amqp.methods.basic.Deliver;
+    import org.amqp.methods.channel.Open;
+    import org.amqp.methods.queue.Declare;
+
+    class TS
+        implements BasicConsumer, 
+        implements LifecycleEventHandler {
+
+        var ax:String ;
+        var q:String ;
+        var q2:String ;
+        var routing_key:String;
+        var connection:Connection;
+        var sessionManager:SessionManager;
+        var sessionHandler:SessionStateHandler;
+        var sessionHandler2:SessionStateHandler;
+
+        var consumerTag:String;
+        var ct:Thread;
+        var mt:Thread;
+        var messages:Deque<BytesInput>;
+
+        public static function main() {
+            var g = new GS();
+            g.run();
+        }
+
+        public function new()
+        {
+            ax = "";
+			q = "q";
+			q2 = "q2";
+            routing_key = q2; // for publish
+
+            connection = new Connection(buildConnectionParams());
+            sessionManager = connection.sessionManager;
+
+            messages = new Deque();
+        }
+
+        public function buildConnectionParams():ConnectionParameters {
+
+            var params:ConnectionParameters = new ConnectionParameters();
+            params.username = "guest";
+            params.password = "guest";
+            params.vhostpath = "/";
+            params.serverhost = "10.0.0.17";
+
+            return params;
+        }
+
+        public function publish(data:Bytes):Void {
+            trace("publish");
+            var publish:Publish = new Publish();
+            publish.exchange = ax;
+            publish.routingkey = routing_key;
+            var props:BasicProperties = Properties.getBasicProperties();
+            var cmd:Command = new Command(publish, props, data);
+            sessionHandler.dispatch(cmd);
+        }
+
+
+        public function run():Void {
+            connection.start();
+            connection.baseSession.registerLifecycleHandler(this);
+            mt =  Thread.current();
+            trace("create connection thread");
+            ct = neko.vm.Thread.create(callback(connection.onSocketData, mt));
+            Thread.readMessage(true); // wait for a start message after onConsume
+
+            runLoop();
+
+            trace("sending close message");
+            ct.sendMessage("close");
+            trace("block till closeOk done");
+            Thread.readMessage(true);
+            trace("run done");
+       }
+
+        public function runLoop():Void {
+            // implement this
+            trace("process in main thread");
+            var msg:BytesInput;
+
+            while(true) {
+                neko.Sys.sleep(0.01);
+                trace("processing main thread "+count);
+                if((msg = messages.pop(false)) != null){
+                    trace("got ping");
+                    var b = new BytesOutput();
+                    b.bigEndian = true;
+                    publish(b.getBytes());
+                }
+            }
+        }
+
+        public function afterOpen():Void {
+            openChannel(setupConsumer);
+        }
+
+        function openChannel(_callback:Dynamic):Void {
+            var whoCares:Dynamic = function(event:ProtocolEvent):Void{
+                trace(event);
+            };
+
+            sessionHandler = sessionManager.create();
+            var open:Open = new Open();
+            var queue:org.amqp.methods.queue.Declare = new org.amqp.methods.queue.Declare();
+            queue.queue = q2;
+            sessionHandler.rpc(new Command(open), whoCares);
+            sessionHandler.rpc(new Command(queue), whoCares);
+
+            sessionHandler2 = sessionManager.create();
+            open = new Open();
+            queue = new org.amqp.methods.queue.Declare();
+            queue.queue = q;
+            sessionHandler2.rpc(new Command(open), whoCares);
+            sessionHandler2.rpc(new Command(queue), _callback);
+        }
+
+        public function setupConsumer(event:ProtocolEvent):Void {
+			trace("setupConsumer");
+            var consume:Consume = new Consume();
+            consume.queue = q;
+            consume.noack = true;
+            sessionHandler2.register(consume, this);
+        }
+
+//        public function cancel(event:TimerEvent):Void {
+   //         log("Initiating cancellation "+consumerTag);
+            //assertNotNull(consumerTag);
+    //        sessionHandler.unregister(consumerTag);
+ //       }
+
+        public function onConsumeOk(tag:String):Void {
+            consumerTag = tag;
+            trace("onConsumeOk: " + tag);
+            mt.sendMessage("start");
+        }
+
+        public function onCancelOk(tag:String):Void {
+            //log("onCancelOk: " + tag);
+        }
+
+        public function onDeliver(method:Deliver,
+                                  properties:BasicProperties,
+                                  body:BytesInput):Void {
+            // this is called by the socket thread
+            messages.add(body);
+        }
+    }
---
+++ sample/timings/Timings.hx
@@ -0,0 +1,155 @@
+	import flash.display.Sprite;
+	import flash.text.TextField;
+
+	import flash.utils.ByteArray;
+    import flash.utils.Timer;
+    import flash.Lib;
+
+	import flash.events.EventDispatcher;
+
+    import org.amqp.Command;
+    import org.amqp.Connection;
+    import org.amqp.ConnectionParameters;
+    import org.amqp.ProtocolEvent;
+    import org.amqp.Session;
+    import org.amqp.SessionManager;
+    import org.amqp.headers.BasicProperties;
+    import org.amqp.impl.SessionStateHandler;
+    import org.amqp.methods.basic.Publish;
+    import org.amqp.methods.channel.Open;
+    import org.amqp.util.Properties;
+
+
+    import org.amqp.BasicConsumer;
+    import org.amqp.Command;
+    import org.amqp.LifecycleEventHandler;
+    import org.amqp.methods.basic.Consume;
+    import org.amqp.methods.basic.Deliver;
+    import org.amqp.methods.channel.Open;
+    import org.amqp.methods.queue.Declare;
+
+    import flash.Vector;
+
+	class Timings implements BasicConsumer, implements LifecycleEventHandler {
+
+        public var ax:String ;
+        public var q:String ;
+        public var q2:String ;
+        public var routing_key:String;
+        public var connection:Connection;
+        public var sessionManager:SessionManager;
+        public var sessionHandler:SessionStateHandler;
+        public var sessionHandler2:SessionStateHandler;
+ 
+        var consumerTag:String;
+
+        var beginTime:Int;
+        var startTime:Int;
+        var endTime:Int;
+        var timings:Vector<Float>;
+
+        static function main() {
+    		var a = new Timings();
+			a.run();
+		}
+
+        public function new()
+        {
+            ax = "";
+			q = "q";
+			q2 = "q2";
+            routing_key = q;
+
+            connection = new Connection(buildConnectionParams());
+            sessionManager = connection.sessionManager;
+
+            timings = new Vector();
+        }
+
+        public function buildConnectionParams():ConnectionParameters {
+            var params:ConnectionParameters = new ConnectionParameters();
+            params.username = "guest";
+            params.password = "guest";
+            params.vhostpath = "/";
+            params.serverhost = "10.0.0.17";
+            return params;
+        }
+
+        public function publish(data:ByteArray):Void {
+            var publish:Publish = new Publish();
+            publish.exchange = ax;
+            publish.routingkey = routing_key;
+            var props:BasicProperties = Properties.getBasicProperties();
+            var cmd:Command = new Command(publish, props, data);
+            sessionHandler.dispatch(cmd);
+        }
+
+        public function run():Void {
+            connection.start();
+            connection.baseSession.registerLifecycleHandler(this);
+        }
+
+        public function afterOpen():Void {
+            openChannel(setupConsumer);
+        }
+
+        public function openChannel(_callback:Dynamic):Void {
+            var whoCares:Dynamic = function(event:ProtocolEvent):Void{
+            };
+
+            sessionHandler = sessionManager.create();
+            var open:Open = new Open();
+            var queue:org.amqp.methods.queue.Declare = new org.amqp.methods.queue.Declare();
+            queue.queue = q;
+            sessionHandler.rpc(new Command(open), whoCares);
+            sessionHandler.rpc(new Command(queue), whoCares);
+
+            sessionHandler2 = sessionManager.create();
+            open = new Open();
+            queue = new org.amqp.methods.queue.Declare();
+            queue.queue = q2;
+            sessionHandler2.rpc(new Command(open), whoCares);
+            sessionHandler2.rpc(new Command(queue), _callback);
+        }
+
+        public function setupConsumer(event:ProtocolEvent):Void {
+            var consume:Consume = new Consume();
+            consume.queue = q2;
+            consume.noack = true;
+            sessionHandler2.register(consume, this);
+        }
+
+//        public function cancel(event:TimerEvent):Void {
+   //         log("Initiating cancellation "+consumerTag);
+            //assertNotNull(consumerTag);
+    //        sessionHandler.unregister(consumerTag);
+ //       }
+
+        public function onConsumeOk(tag:String):Void {
+            consumerTag = tag;
+            trace("onConsumeOk");
+            beginTime = startTime = Lib.getTimer();
+            publish(new ByteArray());
+
+        }
+
+        public function onCancelOk(tag:String):Void {
+        }
+
+        public function onDeliver(method:Deliver,
+                                  properties:BasicProperties,
+                                  body:ByteArray):Void {
+            endTime = Lib.getTimer();
+            timings.push(endTime - startTime);
+            if(endTime < 10000) {
+                startTime = endTime;
+                publish(new ByteArray());
+            } else {
+                var sum:Float = 0;
+                for(t in timings) {
+                    sum += t;
+                }
+                trace("avg roundtrip (ms): "+(sum / timings.length)+" sample time (secs): "+((endTime-beginTime)/1000.0));
+            }
+        }
+    }
